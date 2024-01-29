c7e6546c42611c19b6cf5e8799e04b9de14aa056
juptofex <julien.damilot@gmail.com>
2024-01-29 15:41:06 +0100

    Exo 1 à 2

--- Semaine 1/0-exoplanets_web1_template/routes/index.js
+++ Semaine 1/0-exoplanets_web1_template/routes/index.js
@@ -4,7 +4,15 @@ const router = express.Router();
 /* GET home page. */
 router.get('/', (req, res) => {
   res.render('index.hbs');
+  console.log("No problem, everything is working well")
 });
 
+router.get('/exolunes', (req, res) => {
+  let exolunes_list = ["DH Tauri", "Kepler-409", "WASP-49"];
+  let date =  new Date().toDateString() ;
+  let time = new Date().toTimeString();
+  let dateAct = "Nous sommes le " + date + ". Il est "+ time + "à Bruxelles."
+  res.render('exolunes.hbs', {dateAct : dateAct, exolist : exolunes_list});
+})
 
 module.exports = router;
