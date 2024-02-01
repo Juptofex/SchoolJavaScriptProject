bbbd22207a7165d0fcfb6a972abffa2d74c1e4bb
juptofex <julien.damilot@gmail.com>
2024-02-01 11:08:46 +0100

    EXO DONE

--- Semaine 1/0-exoplanets_web1_template/routes/index.js
+++ Semaine 1/0-exoplanets_web1_template/routes/index.js
@@ -15,4 +15,26 @@ router.get('/exolunes', (req, res) => {
   res.render('exolunes.hbs', {dateAct : dateAct, exolist : exolunes_list});
 })
 
+router.get('/telescope', (req, res) => {
+  //Can be done in another way (dictionnaries)
+  /*Like this way : 
+  let telescope = [];
+  let spain = {name : 'Gran Telescopio Canarias', countries : 'Espagne', diameter : 10.4}
+  let usa = {name : 'Keck 1', countries : 'Etats-Unis', diameter : 9.8}
+  let japan = {name : 'Seimei', countries : 'Japon', diameter : 3.8}
+  telescope.push(spain, usa, japan)
+  */
+  let table = [
+    ['Gran Telescopio Canarias', 'Espagne', 10.4],
+    ['Keck 1', 'Etats-Unis', 9.8],
+    ['Seimei', 'Japon', 3.8]
+  ];
+  let errorMess = 'Il faut au minimum 3 téléscope dans le tableau';
+  if (table.length<3) {
+    res.render('telescope.hbs', {teleTable : table, error : errorMess});
+  } else {
+    res.render('telescope.hbs', {teleTable : table});
+  }
+})
+
 module.exports = router;
